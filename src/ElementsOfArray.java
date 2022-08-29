2f92ff26e6c37f1f3777352e1b2a15b45df22448
srushti-Mane <srushtideshmukh2909@gmail.com>
2022-08-29 10:17:17 +0530

    added:welcome to array

---
+++ src/ElementsOfArray.java
@@ -0,0 +1,9 @@
+public class ElementsOfArray {
+    public static void main(String[] args){
+        int arr[]={1,2,3,4,5,6,7,8,9,10};
+                System.out.println("elements of array are : ");
+        for (int s = 0; s < arr.length; s++) {
+            System.out.print(arr[s] + " ");
+        }
+    }
+}
